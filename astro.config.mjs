b218c19def31d26668e2cd70d4e0bf143915cb37
Bryan Juanacio <bryanjuanacio@Mac-mini-de-Bryan.local>
2025-05-03 16:41:51 -0500

    fix: config vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel/static'; 
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
+  adapter: vercel({}),
   vite: {
     plugins: [tailwindcss()]
   }
